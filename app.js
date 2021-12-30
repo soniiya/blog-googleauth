d2f4be921eb8c4df5b0bc85f282757d681999690
Soniya Jaiswal <91732415+soniiya@users.noreply.github.com>
2021-12-30 13:03:21 +0530

    added auth

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -19,15 +22,48 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const uri=process.env.MONGO_URI;
-const options={
+app.use(session({
+  secret: "success",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+const uri = process.env.DB_PATH;
+const options = {
   useNewUrlParser: true,
-}
-mongoose.connect(uri,options);
+  useUnifiedTopology:true,
+};
+ mongoose.connect(uri, options);
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
 const postSchema = {
   title: String,
-  content: String
+  content: String,
+
 };
 
 const Post = mongoose.model("Post", postSchema);
@@ -93,12 +129,58 @@ const requestedPostId = req.params.postId;
   });
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+
+
+
+app.get("/login", function (req, res) {
+  res.render("login");
+})
+
+app.get("/register", function (req, res) {
+  res.render("register");
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+})
+
+app.get("/success", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("success");
+  } else {
+    res.redirect("/login");
+  }
+})
+
+app.post("/register", function (req, res) {
+  User.register({ username: req.body.username },
+    req.body.password, function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/success");
+        });
+      }
+    });
+});
+
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    passport: req.body.password
+  });
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/success");
+      });
+    }
+  })
 });
 
 app.listen(process.env.PORT || 5000 , function() {
